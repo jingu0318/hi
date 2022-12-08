44852554da607a4260bb6e5fe4115ece84d17f7f
jingu lee <18dlwlsrn@naver.com>
2022-12-08 12:51:48 +0900

    학번 이메일 입력창

--- public/js/index.js
+++ public/js/index.js
@@ -5,18 +5,32 @@ const localUserName = localStorage.getItem('userName');
 
 
 const setUserNameInnerHtml = (name) => {
-  headerUserNameElement.innerHTML = '${name} <span>님</span>';
+  headerUserNameElement.innerHTML = `${name} <span>님</span>`;
   userNameElement.textContent = name;
 };
 
-if (localuserName) {
-  setUserNameInnerHtml(localuserName);
+if (localUserName) {
+  setUserNameInnerHtml(localUserName);
 }
 
 userNameElement.onclick = () =>{
   const userName = prompt('이름을 입력해 주세요.');
-  //todo: userName에 대한 유효성 검사
+  // todo: userName에 대한 유효성 검사
   localStorage.setItem('userName', userName);
 
   setUserNameInnerHtml(userName);
+};
+
+
+const userEmailElement = document.querySelector('uesr-email');
+const localUserEmail = localStorage.getItem('userEmail');
+
+if (localUserEmail) {
+  userEmailElement.innerHTML = `${localUserEmail}`;
+}
+
+userEmailElement.onclick = () =>{
+  const userEmail = prompt('학번과 이메일을 입력하세요');
+  localStorage.setItem('userEmail', userEmail);
+  userEmailElement.innerHTML = `${userEmail}`;
 };
