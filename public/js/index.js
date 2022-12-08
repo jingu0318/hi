27ae811944f4a50bff9303ec94e372f04fab9f88
jingu lee <18dlwlsrn@naver.com>
2022-12-08 11:40:42 +0900

    사용자 입름 입력 받는 기능 추가

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,19 @@
-const test = 10;
+const headerUserNameElement = document.querySelector('.user');
+const userNameElement = document.querySelector('.user_name');
 
-console.log(test);
+const setUserNameInnerHtml = (name) => {
+  headerUserNameElement.innerHTML = '${name} <span>님</span>';
+  userNameElement.innerHTML = '${name} <span>님</span>';
+};
+
+if(localuserName){
+  setUserNameInnerHtml(localuserName);
+}; 
+
+userNameElement.onclick = () =>{
+  const userName = prompt('이름을 입력해 주세요.');
+  //todo: userName에 대한 유효성 검사
+  localStorage.setItem('userName', userName);
+
+  setUserNameInnerHtml(userName);
+};
