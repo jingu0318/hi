516f5b9a08a7e8adb43e327c5dfe1d3a911cfcfd
jingu lee <18dlwlsrn@naver.com>
2022-12-08 11:54:00 +0900

    클릭버튼 오류 수정

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,17 @@
 const headerUserNameElement = document.querySelector('.user');
-const userNameElement = document.querySelector('.user_name');
+const userNameElement = document.querySelector('#name');
+
+const localUserName = localStorage.getItem('userName');
+
 
 const setUserNameInnerHtml = (name) => {
   headerUserNameElement.innerHTML = '${name} <span>님</span>';
-  userNameElement.innerHTML = '${name} <span>님</span>';
+  userNameElement.textContent = name;
 };
 
-if(localuserName){
+if (localuserName) {
   setUserNameInnerHtml(localuserName);
-}; 
+}
 
 userNameElement.onclick = () =>{
   const userName = prompt('이름을 입력해 주세요.');
